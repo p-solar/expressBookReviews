04e01a3403a4d85eada01f672c82185d4c0db8ac
Prasanna Rotti <protti@enphaseenergy.com>
2023-01-02 22:40:24 -0800

    added async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const axios = require('axios')
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -23,45 +24,54 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  
-  res.send(JSON.stringify(books,null,4))
+public_users.get('/', async function (req, res) {
+    const bookdb = await books;
+   res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   const isbn = req.params.isbn;
-  
+  const book = await books[isbn];
 
-  res.send(books[isbn])
+  res.send(book)
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author
+  let book = {};
   let keys = Object.keys(books);
-  console.log(keys)
-  keys.forEach((key)=>{
-    if(books[key].author === author) {
-      res.send(books[key])
-    }
-  })
-  res.send('book not found')
+  book = await getbookByAuthor(author,keys);
+  
+  res.send(book)
 });
 
+ function getbookByAuthor(author,keys) {
+  console.log(author + keys)
+  const key = keys.filter((key)=> books[key].author===author)
+  console.log(books[key])
+  return books[key];
+  
+}
+
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //Write your code here
+  let book = {};
   const title = req.params.title
   let keys = Object.keys(books);
-  console.log(keys)
-  keys.forEach((key)=>{
-    if(books[key].title === title) {
-      res.send(books[key])
-    }
-  })
-  res.send('book not found')
+  book = await getbookByTitle(title,keys);
+  res.send(book)
 });
+
+function getbookByTitle(title,keys) {
+  console.log(title + keys)
+  const key = keys.filter((key)=> books[key].title===title)
+  console.log(books[key])
+  return books[key];
+  
+}
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
